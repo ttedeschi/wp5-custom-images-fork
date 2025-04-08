76af733df6e3aacd4d7bf0f5a9473e9778e1593b
Tommaso Tedeschi <tedesk95@gmail.com>
2025-04-08 12:53:37 +0200

    Update refresh-manifest.py

--- jupyterlab/refresh-manifest.py
+++ jupyterlab/refresh-manifest.py
@@ -8,17 +8,17 @@ def generate_random_suffix(length=5):
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
 
 
-while(False):
-    with open("/opt/conda/lib/python3.11/site-packages/dask_labextension/DaskCluster.yaml") as f:
-        manifest = yaml.safe_load(f)
+while(True):
+    #with open("/opt/conda/lib/python3.11/site-packages/dask_labextension/DaskCluster.yaml") as f:
+    #    manifest = yaml.safe_load(f)
 
-    random_suffix = generate_random_suffix()
-    manifest["metadata"]["name"] =  "dask-" + os.environ.get("USERNAME") + "-" + random_suffix
-    manifest["spec"]["scheduler"]["service"]["selector"]["dask.org/cluster-name"] = "dask-" + os.environ.get("USERNAME") + "-" + random_suffix
-    manifest["spec"]["scheduler"]["spec"]["containers"][0]["image"] = "ghcr.io/icsc-spoke2-repo/jlab:wp5-alma9-" + os.environ.get("IMAGE_TAG") 
-    manifest["spec"]["worker"]["spec"]["containers"][0]["image"] = "ghcr.io/icsc-spoke2-repo/jlab:wp5-alma9-" + os.environ.get("IMAGE_TAG") 
+    #random_suffix = generate_random_suffix()
+    #manifest["metadata"]["name"] =  "dask-" + os.environ.get("USERNAME") + "-" + random_suffix
+    #manifest["spec"]["scheduler"]["service"]["selector"]["dask.org/cluster-name"] = "dask-" + os.environ.get("USERNAME") + "-" + random_suffix
+    #manifest["spec"]["scheduler"]["spec"]["containers"][0]["image"] = "ghcr.io/icsc-spoke2-repo/jlab:wp5-alma9-" + os.environ.get("IMAGE_TAG") 
+    #manifest["spec"]["worker"]["spec"]["containers"][0]["image"] = "ghcr.io/icsc-spoke2-repo/jlab:wp5-alma9-" + os.environ.get("IMAGE_TAG") 
 
-    with open("/opt/conda/lib/python3.11/site-packages/dask_labextension/DaskCluster.yaml", "w") as f:
-        yaml.dump(manifest, f)
+    #with open("/opt/conda/lib/python3.11/site-packages/dask_labextension/DaskCluster.yaml", "w") as f:
+    #    yaml.dump(manifest, f)
 
     sleep(10)
