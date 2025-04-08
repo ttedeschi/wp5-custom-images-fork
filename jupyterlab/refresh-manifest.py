7ddf3d00563cadc9357f29fccf33e77dca51d4c9
Tommaso Tedeschi <tedesk95@gmail.com>
2025-04-08 11:08:22 +0200

    disable refresh manifest

--- jupyterlab/refresh-manifest.py
+++ jupyterlab/refresh-manifest.py
@@ -8,7 +8,7 @@ def generate_random_suffix(length=5):
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
 
 
-while(True):
+while(False):
     with open("/opt/conda/lib/python3.11/site-packages/dask_labextension/DaskCluster.yaml") as f:
         manifest = yaml.safe_load(f)
 
