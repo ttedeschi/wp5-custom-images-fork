2af6610041b42a08fa445f0ac50abf9c98d994f9
Tommaso Tedeschi <tedesk95@gmail.com>
2025-06-11 16:27:20 +0200

    Update update_daskworkergroups.py

--- jupyterlab/update_daskworkergroups.py
+++ jupyterlab/update_daskworkergroups.py
@@ -8,6 +8,8 @@ custom_api = client.CustomObjectsApi()
 
 namespace = "jhub"
 
+updated = []
+
 while True:
     worker_groups = custom_api.list_namespaced_custom_object(
     group="kubernetes.dask.org",
@@ -16,30 +18,34 @@ while True:
     plural="daskworkergroups"
     )
     for item in worker_groups["items"]:
-        try:
-            name_complete = item["metadata"]["name"]
-            name = item["metadata"]["name"][:-8] #remove -default
-            print(f"Modifying env for: {name}")
+        name_complete = item["metadata"]["name"]
+        name = item["metadata"]["name"][:-8] #remove -default
+        if name not in updated: 
+            try: 
+                print(f"Modifying env for: {name}")
+                
+                containers = item["spec"]["worker"]["spec"]["containers"]
+                container = containers[0]
             
-            containers = item["spec"]["worker"]["spec"]["containers"]
-            container = containers[0]
-        
-            port = int(container["env"][0]["value"].split(":")[-1]) + 2
-            ip_url = "http://" + name + "-scheduler.jhub:" + str(port) + "/ip"
-            response = requests.get(ip_url)
-            data = response.json()     
-            ip = data["private_ip"].strip()
-        
-            container["env"][0]["value"] = container["env"][0]["value"].replace("localhost", ip) 
-        
-            custom_api.patch_namespaced_custom_object(
-                group="kubernetes.dask.org",
-                version="v1",
-                namespace=namespace,
-                plural="daskworkergroups",
-                name=name_complete,
-                body=item
-            )
-        except:
-            pass
+                port = int(container["env"][0]["value"].split(":")[-1]) + 2
+                ip_url = "http://" + name + "-scheduler.jhub:" + str(port) + "/ip"
+                response = requests.get(ip_url)
+                data = response.json()     
+                ip = data["private_ip"].strip()
+            
+                container["env"][0]["value"] = container["env"][0]["value"].replace("localhost", ip) 
+            
+                custom_api.patch_namespaced_custom_object(
+                    group="kubernetes.dask.org",
+                    version="v1",
+                    namespace=namespace,
+                    plural="daskworkergroups",
+                    name=name_complete,
+                    body=item
+                )
+                
+                updated.append(name)
+            except:
+                pass
+                
         time.sleep(1)
