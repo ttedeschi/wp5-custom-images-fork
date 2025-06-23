490d8a334b83e2ab2e545c63492ab8792a6e3c33
Tommaso Tedeschi <tedesk95@gmail.com>
2025-06-23 18:13:39 +0200

    Update refresh-manifest.py

--- jupyterlab/refresh-manifest.py
+++ jupyterlab/refresh-manifest.py
@@ -30,8 +30,8 @@ while(True):
 
     #manifest["spec"]["scheduler"]["spec"]["containers"][0]["image"] = "ghcr.io/icsc-spoke2-repo/jlab:wp5-alma9-" + os.environ.get("IMAGE_TAG") 
     #manifest["spec"]["worker"]["spec"]["containers"][0]["image"] = "ghcr.io/icsc-spoke2-repo/jlab:wp5-alma9-" + os.environ.get("IMAGE_TAG") 
-    manifest["spec"]["scheduler"]["spec"]["containers"][0]["image"] = "ghcr.io/ttedeschi/jlab:wp5-alma9-test-vk-kbfi" 
-    manifest["spec"]["worker"]["spec"]["containers"][0]["image"] = "ghcr.io/ttedeschi/jlab:wp5-alma9-test-vk-kbfi" 
+    manifest["spec"]["scheduler"]["spec"]["containers"][0]["image"] = "/cvmfs/unpacked.cern.ch/ghcr.io/comp-dev-cms-ita/jupyterlab:v2-offload-highrate-v0.0.1" 
+    manifest["spec"]["worker"]["spec"]["containers"][0]["image"] = "/cvmfs/unpacked.cern.ch/ghcr.io/comp-dev-cms-ita/jupyterlab:v2-offload-highrate-v0.0.1" 
     manifest["spec"]["scheduler"]["spec"]['containers'][0]["env"][0]['value'] = os.getenv("JUPYTERHUB_USER")
     manifest["spec"]["scheduler"]["spec"]['containers'][0]["env"][1]['value'] = os.getenv("JUPYTERHUB_API_TOKEN")
 
